5f754397810ff05d1690161769ba8891c9d16504
nikawadori <nikawa.2161.t@gmail.com>
2022-07-13 00:01:23 +0900

    unitテスト実行

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,14 +8,14 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
      *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function 一覧を取得()
     {
-        $response = $this->get('/');
+        $response = $this->getJson('api/tasks');
 
-        $response->assertStatus(200);
+        $response
+                ->assertOk();
     }
 }
