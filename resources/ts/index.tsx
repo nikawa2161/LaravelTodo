4bae72f183a2e5fca6ad6ad890b98f70acde754f
nikawadori <nikawa.2161.t@gmail.com>
2022-07-19 21:49:51 +0900

    APIでGETの処理

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import App from './app';
 
 
-ReactDOM.render(<App />,document.getElementById('app'))
+// ReactDOM.render(<App />,document.getElementById('app'))
+
+    const container = document.getElementById("app");
+    if (container) {
+        const root = createRoot(container);
+        root.render(<App />);
+    }
